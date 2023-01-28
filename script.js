8ef22972cfc6be20901ce3bbba8ed68b518647d4
teamidol <laurencemy@gmail.com>
2023-01-27 18:39:37 -0800

    loop

--- script.js
+++ script.js
@@ -35,31 +35,31 @@ interactiveGame(userInput)
 
 //vvvvvvvvvvvvvvvvvvv ( lab08 ) vvvvvvvvvvvvvvvvvv
 
-// let userPick = prompt("Pick a number between 1 and 5");
-// console.log(userPick);
+let userPick = prompt("Pick a number between 1 and 5 (images to show)");
+console.log(userPick);
+userPick = parseInt(userPick)
+for (let i = 0; i < userPick; i += 1){
+    document.write('<img src="thumb wrestling sml.png"/>')
+}
 
 // function pickNumber(userPick){
-    
-
 
 //     let gotIt = false;
 //     while (!gotIt) {
 
-//         if (userPick == 1) {
+//         if (userPick == '1') {
 //             document.write (img src= "thumb wrestling.png")
 //             document.write (img src="thumb wrestling.png");
 //             gotIt = true
-//         } else if (userInput == "rock") {
+//         } else if (userInput == '2') {
 //             alert("im paper");
 //             gotIt = true
 //         } else if (userInput == "scissor") {
 //             alert("im rock");
 //             gotIt = true
 //         } else {
-//             userInput = prompt("Type 'paper' or 'rock' or 'scissor'")
-        
+//             userInput = prompt("Type 'paper' or 'rock' or 'scissor'")     
 //         }
-
 //     }
 // }
 // pickNumber(userPick)
